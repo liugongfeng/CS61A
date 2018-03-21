60e5e91af77cf8d3d0228f2ae9bdece100eb41bf
liugongfeng <liugong.feng@aliyun.com>
2018-03-21 15:16:49 +0800

    Add files via upload

---
+++ lab06_extra.py
@@ -0,0 +1,30 @@
+""" Optional problems for Lab 6 """
+
+## Nonlocal practice
+def vending_machine(snacks):
+    """Cycles through sequence of snacks.
+
+    >>> vender = vending_machine(('chips', 'chocolate', 'popcorn'))
+    >>> vender()
+    'chips'
+    >>> vender()
+    'chocolate'
+    >>> vender()
+    'popcorn'
+    >>> vender()
+    'chips'
+    >>> other = vending_machine(('brownie',))
+    >>> other()
+    'brownie'
+    >>> vender()
+    'chocolate'
+    """
+    "*** YOUR CODE HERE ***"
+    index = 0
+    def vender():
+        nonlocal index
+        result = snacks[index]
+        index = (index + 1) % len(snacks)
+        return result
+
+    return vender
